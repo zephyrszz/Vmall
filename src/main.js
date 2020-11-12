a534ea574e15dddc393e8eff232d8db1a19b44d6
zephyrszz <2424459079@qq.com>
2020-11-12 17:10:49 +0800

    1

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App.vue';
+
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false
 
 new Vue({
